1aa6adfc96d8be3e6892f35b87070d6289712bc5
jmaveety <jmaveety@caresense.com>
2019-12-29 12:44:40 -0800

    Updates

---
+++ precache-manifest.3eaeac57fe97ac74cf154d2ed522b8b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9556f39a48f52719f44eecd73ef55160",
+    "url": "/midgets/index.html"
+  },
+  {
+    "revision": "49a4aa6372de9a49ba7f",
+    "url": "/midgets/static/css/main.4a2ac423.chunk.css"
+  },
+  {
+    "revision": "ead3cf65f53e0e77b1f1",
+    "url": "/midgets/static/js/2.0fdae663.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/midgets/static/js/2.0fdae663.chunk.js.LICENSE"
+  },
+  {
+    "revision": "49a4aa6372de9a49ba7f",
+    "url": "/midgets/static/js/main.8b269882.chunk.js"
+  },
+  {
+    "revision": "8162d09e96151d3db74e",
+    "url": "/midgets/static/js/runtime-main.47cf5419.js"
+  },
+  {
+    "revision": "4ff563ba1f22e26cad0be5cc1509d4e0",
+    "url": "/midgets/static/media/moms.4ff563ba.jpg"
+  }
+]);
